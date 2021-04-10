b6122da89d2a52e3087791d2e7fad5c29dfcc562
Jamie Houseman <jamie@etg-software.co.uk>
2021-04-10 14:26:54 +0100

    Add player class

---
+++ DZ-Game/GameObjects/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace DZGame.GameObjects
+{
+    public class Player
+    {
+        public int Position_X { get; set; }
+        public int Position_Y { get; set; }
+        public int Position_Z { get; set; }
+
+        public Player(int x, int y, int z)
+        {
+            Position_X = x;
+            Position_Y = y;
+            Position_Z = z;
+        }
+    }
+}
